21f2f4754f998e2852eca672c4a8528083944502
Yaying <yliangli20@stuy.edu>
2020-12-01 23:53:45 -0500

    finished part 2

--- code1.java
+++ code1.java
@@ -8,13 +8,31 @@ public class code1 {
   public static void main(String[] args) {
     ArrayList<Integer> all = new ArrayList<>();
     ReadFile(all); //add all the numbers into the ArrayList all
+    boolean printed = false; //so i don't print results a bajillion times! :D
     for (int i = 0; i < all.size(); i++) {
-      if (all.indexOf(2020-all.get(i)) != -1) {
+      if (!printed && all.indexOf(2020-all.get(i)) != -1) {
+        printed = true;
         int firstNumb = all.get(i);
         int secondNumb = all.get(all.indexOf(2020-firstNumb));
         System.out.println("First number" + firstNumb);
         System.out.println("Second number" + secondNumb);
         System.out.println(firstNumb*secondNumb);
+      }
+    }
+    printed = false;
+    System.out.println("~~~~~~~~~~~~~~~~");
+    for (int i = 0; i < all.size(); i++) {
+      for (int j = 1; j < all.size(); j++) {
+        if (!printed && all.get(i) + all.get(j) < 2020 && all.indexOf(2020-(all.get(i)+all.get(j))) != -1) {
+          printed = true;
+          int firstNumber = all.get(i);
+          int secondNumber = all.get(j);
+          int thirdNumber = all.get(all.indexOf(2020-(firstNumber+secondNumber)));
+          System.out.println("First number" + firstNumber);
+          System.out.println("Second number" + secondNumber);
+          System.out.println("Third number" + thirdNumber);
+          System.out.println(firstNumber*secondNumber*thirdNumber);
+        }
       }
     }
   }
