73fe7ead50f76bad52ecb8f58cc5b312fb91d167
Yaying <yliangli20@stuy.edu>
2020-12-07 09:16:54 -0500

    finally fixed part 2 arghh

--- code3.java
+++ code3.java
@@ -5,18 +5,29 @@ public class code3{
 
   public static void main(String[] args) {
     try {
-      Scanner myReader = new Scanner(new File("input3.txt"));
-      int row = 0;
-      int trees = 0;
-      while (myReader.hasNextLine()) {
-        String data = myReader.nextLine();
-        if (data.charAt(row*3%31) == '#') {
-          trees++;
+      double[] slopes = new double[] {1.0, 3.0, 5.0, 7.0, 0.5};
+      long multAllTrees = 1;
+      for (int i = 0; i < slopes.length; i++) {
+        Scanner myReader = new Scanner(new File("input3.txt"));
+        int row = 0;
+        int trees = 0;
+        while (myReader.hasNextLine()) {
+          String data = myReader.nextLine();
+          if (i == slopes.length-1 && row%2 != 0) {
+            row++;
+            continue;
+          }
+          if (data.charAt((int)(row*slopes[i])%data.length()) == '#') {
+            trees++;
+          }
+          row++;
         }
-        row++;
+        System.out.println(trees);
+        multAllTrees *= trees;
+        myReader.close();
       }
-      System.out.println(trees);
-      myReader.close();
+      System.out.println(multAllTrees);
+      //System.out.println(85*176*96*87*400); -1561239552
     } catch (FileNotFoundException e) {
       System.out.println("An error occurred.");
     }
